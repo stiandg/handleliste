9fce3972edf5107e7d04c26ea39e01afe48a986c
stian <github@stian.no>
2012-03-09 13:43:35 +0100

    first one!

---
+++ Handleliste/Models/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+    public class Item
+    {
+        public Item() { }
+
+        public Item(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+}
