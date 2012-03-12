9c41e7a04c7281f53724933c29b3ac75df1a50c5
stian <github@stian.no>
2012-03-12 16:34:39 +0100

    added web api

---
+++ Handleliste/Controllers/ItemsController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using Handleliste.Models;
+
+namespace Handleliste.Controllers
+{
+    public class ItemsController : ApiController
+    {
+        private static List<Item> items = new List<Item> { new Item("melk"), new Item("brød") };
+
+        // GET /api/items
+        public IEnumerable<Item> Get()
+        {
+            return items;
+        }
+
+        // GET /api/items/5
+        public string Get(int id)
+        {
+            return "melk";
+        }
+
+        // POST /api/items
+        public void Post(Item newItem)
+        {
+            items.Add(newItem);
+        }
+
+        // PUT /api/items/5
+        public void Put(int id, string value)
+        {
+        }
+
+        // DELETE /api/items/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Handleliste/Models/Items.cs
@@ -0,0 +1,14 @@
+﻿namespace Handleliste.Models
+{
+    public class Item
+    {
+        public Item() { }
+
+        public Item(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+}
